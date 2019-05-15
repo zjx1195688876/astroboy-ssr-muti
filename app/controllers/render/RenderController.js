241d5ffb3a9bbf973d6ecc44432e743f86ada2d7
zjx1195688876 <15757116071@163.com>
2019-05-15 10:48:50 +0800

    feat: fix

--- app/controllers/render/RenderController.js
+++ app/controllers/render/RenderController.js
@@ -1,20 +1,8 @@
-// const path = require('path');
-// const fs = require('fs');
-const H5BaseController = require('@youzan/iron-base/app/controllers/base/H5BaseController');
+const BaseController = require('BaseController');
 
-class RenderController extends H5BaseController {
-  async getMasterDetailHtml(ctx) {
-    const html = await ctx.renderSSR('master-detail');
-    ctx.body = html;
-  }
-
-  async getMasterDetailHtml2(ctx) {
-    const html = await ctx.renderSSR('master-detail2');
-    ctx.body = html;
-  }
-
-  async getDetailHtml(ctx) {
-    const html = await ctx.renderSSR('detail');
+class RenderController extends BaseController {
+  async getHtml(ctx) {
+    const html = await ctx.renderSSR();
     ctx.body = html;
   }
 }
